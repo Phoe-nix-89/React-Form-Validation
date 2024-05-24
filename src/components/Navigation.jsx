5282df4652ad740b3c9c8e6c85247f77067f1566
Phoe-nix-89 <sonkhoseth074@gmail.com>
2024-05-24 07:20:47 +0530

    Minute Changes

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,3 @@
-/*-------------------------------------------------------------------
-|  🐼 React FC Navigation
-|
-|  🐯 Purpose: TOP NAVIGATION BAR
-|
-|  🐸 Returns:  JSX
-*-------------------------------------------------------------------*/
-
 import React from 'react'
 import { BsGithub, BsTwitter } from 'react-icons/bs'
 
@@ -15,10 +7,10 @@ export const Navigation = () => {
       <nav className="container flex justify-between items-center">
         <div className="font-semibold">React Forms</div>
         <div className="flex items-center gap-4 text-lg">
-          <a href="https://github.com/Yazdun/react-fcc-forms" target="_blank">
+          <a href="https://github.com/Phoe-nix-89/React-Form-Validation" target="_blank">
             <BsGithub />
           </a>
-          <a href="https://twitter.com/Yazdun" target="_blank">
+          <a href="#" target="_blank">
             <BsTwitter />
           </a>
         </div>
